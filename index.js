8c6f170bb288d8103cb4416d78f80dfc40aece08
GuitherezDika <guitherez.git@gmail.com>
2024-07-04 09:12:49 +0700

    parsed client data 2

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const productRoutes = require('./src/routes/products');
 // middleware handle parsing form data client
 // extended = true -> diperbolehkan nested object
 // CARA 1 -> di set agar router bisa akses client data
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
 
+// CARA 2 - parsed data dengan third party
+app.use(bodyParser.json())
 // set header - prevent CORS error
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', "*") // * bisa di akses oleh semua URL
