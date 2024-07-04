76d7c8c99f6ecae518b07ed51f8daf5a974ba392
GuitherezDika <guitherez.git@gmail.com>
2024-07-04 07:51:20 +0700

    set header and handle cors

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const productRoutes = require('./src/routes/products');
 
-app.use('/', productRoutes);
+// set header - prevent CORS error
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*") // * bisa di akses oleh semua URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
+// harus terakhir sebelum action listen
+app.use('/v1/customer', productRoutes);
 
 app.listen(3000, () => {
     console.log('server run on port 3000');
