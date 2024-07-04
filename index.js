07e3f90dfa065d2d5f82924fa3315c46af4b7b07
GuitherezDika <guitherez.git@gmail.com>
2024-07-04 10:39:22 +0700

    registration api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 
 const app = express();
 const productRoutes = require('./src/routes/products');
+const authRoutes = require('./src/routes/auth');
 
 // CARA 1 - parsed data dengan third party
 app.use(bodyParser.json())
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // harus terakhir sebelum action listen
 app.use('/v1/customer', productRoutes);
+app.use('/v1/auth', authRoutes)
 
 app.listen(3000, () => {
     console.log('server run on port 3000');
