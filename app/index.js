20121c2f553f3a378a9a3f4fe11cda175a9852e2
GuitherezDika <guitherez.git@gmail.com>
2024-07-23 16:50:41 +0700

    init setup vercel

---
+++ app/index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectToDatabase = require('../config');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+
+connectToDatabase();
+
+const authRoutes = require('../src/routes/auth');
+const blogRoutes = require('../src/routes/blog');
+
+// upload image
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(
+            null, // null = success
+            'images'// direktori simpan file pada server
+        )
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        // 1E9 = 1 miliar => 0.12345 * 1 miliar = 123450000
+        cb(null, uniqueSuffix+'-'+ file.originalname)
+    },
+})
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype == 'image/jpg' || file.mimetype == 'image/png') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: {
+        fieldNameSize: 100, // 100 karakter
+        fileSize: 1024 * 1024 * 5, // 5 Mb
+        files: 4, // jumlah file upload = 4
+        fields: 4, // jumlah field ikutaan maksimal 4 (misal name, email, phone, color)
+        parts: 5, // form data multipart yang di upload harus kurang dari 5 form
+        headerPairs: 4, // max number of header key -> jumlah pasangan header yang diterima dari permintaan HTTP saat mengunggah file menggunakan form-data multipart
+    }
+})
+
+app.use(upload.single('image'))
+app.use('../images', express.static(path.join(__dirname, 'images')));
+
+// CARA 1 - parsed data dengan third party
+app.use(bodyParser.json())
+
+// set header - prevent CORS error
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*") // * bisa di akses oleh semua URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
+// harus terakhir sebelum action listen
+app.use('/v1/auth', authRoutes)
+app.use('/v1/blog', blogRoutes)
+
+// handle global error from all routes
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 400;
+    const errMessage = err.message || 'invalid';
+    const data = err.data || [];
+
+    if (err instanceof multer.MulterError) {
+        switch (err.code) {
+            case 'LIMIT_UNEXPECTED_FILE':
+                return res.status(400).json({ message: 'Unexpected file number, maximum number of file is one.', data: [] });
+            case 'LIMIT_PART_COUNT':
+                return res.status(400).json({ message: err.message, data: [] });
+            default:
+                return res.status(400).json({ message: err.message, data: [] })
+        }
+    } else {
+        res.status(errorStatus).json({
+            message: errMessage,
+            data
+        })
+    }
+})
+
+app.listen(3000, () => {
+    console.log('server run on port 3000');
+})
