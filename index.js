09460d3ae072b3c261f5d01ead723028c07b0288
GuitherezDika <guitherez.git@gmail.com>
2024-07-24 11:06:06 +0700

    change root index

--- index.js
+++ index.js
@@ -87,6 +87,6 @@ app.use((err, req, res, next) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log('server run on port 3000');
+app.listen(3001, () => {
+    console.log('server run on port 3001');
 })
